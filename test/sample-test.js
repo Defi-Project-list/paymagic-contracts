794c4d2b50234a22fcc65f0e4eb55bed164b715b
de33 <davideiber@gmail.com>
2021-09-21 18:56:49 -0400

    Add TestERC20.sol and disperseToken test
    Add smart contract for test ERC20 to be used in disperseToken function test

--- test/sample-test.js
+++ test/sample-test.js
@@ -37,3 +37,56 @@ describe("disperseEther", function () {
     );
   });
 });
+
+describe("disperseToken", function () {
+  let addr1, addr2, addr3, disperse, testERC20;
+
+  beforeEach(async () => {
+    const Disperse = await ethers.getContractFactory("Disperse");
+    disperse = await Disperse.deploy();
+    await disperse.deployed();
+
+    const TestERC20 = await ethers.getContractFactory("TestERC20");
+    testERC20 = await TestERC20.deploy("PayMagic", "PYMG", 1000);
+    await testERC20.deployed();
+
+    [addr1, addr2, addr3] = await ethers.getSigners();
+  });
+
+  it("Should allow Address 1 to send 500 PYMG tokens to Address 2 and 500 PYMG tokens to Address 3", async function () {
+    const addr1Balance = await testERC20.balanceOf(addr1.address);
+    expect(addr1Balance).equal(1000);
+
+    const addr2Balance = await testERC20.balanceOf(addr2.address);
+    expect(addr2Balance).equal(0);
+
+    const addr3Balance = await testERC20.balanceOf(addr3.address);
+    expect(addr3Balance).equal(0);
+
+    console.log(testERC20.address);
+    // await testERC20.approve(addr1.address, 1100);
+    // const allowance = await testERC20.allowance(addr1.address, addr1.address);
+
+    await testERC20.approve(disperse.address, 1000);
+    // await testERC20.approve(addr3.address, 500);
+
+    await testERC20.allowance(addr1.address, addr2.address);
+    // await testERC20.allowance(addr3.address, addr1.address);
+
+    const tx = await disperse.disperseToken(
+      testERC20.address,
+      [addr2.address, addr3.address],
+      [500, 500]
+    );
+
+    await tx.wait();
+
+    const addr2BalanceNew = await testERC20.balanceOf(addr2.address);
+    const addr2BalanceNewNum = addr2BalanceNew.toNumber();
+    expect(addr2BalanceNewNum).equal(500);
+
+    const addr3BalanceNew = await testERC20.balanceOf(addr3.address);
+    const addr3BalanceNewNum = addr3BalanceNew.toNumber();
+    expect(addr3BalanceNewNum).equal(500);
+  });
+});
