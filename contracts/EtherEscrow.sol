4a0f4f687050f4fd1df24b55aaed01d0bc08c6fd
de33 <davideiber@gmail.com>
2021-10-12 09:01:23 -0400

    Add updated EtherEscrow and IWETHGateway

--- contracts/EtherEscrow.sol
+++ contracts/EtherEscrow.sol
@@ -1,38 +1,40 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import './interfaces/IWETHGateway.sol';
 
-interface IWETHGateway {
-  function depositETH(address onBehalfOf, uint16 referralCode) external payable;
-  function withdrawETH(uint256 amount, address to) external;
+contract EtherEscrow {
+    address arbiter;
+    address depositor;
+    address beneficiary;
+    uint initialDeposit;
+    IWETHGateway gateway = IWETHGateway(0xDcD33426BA191383f1c9B431A342498fdac73488);
+    IERC20 aWETH = IERC20(0x030bA81f1c18d280636F32af80b9AAd02Cf0854e);
+    
+    // Aave referral code - currently unused
+    uint16 referralCode = 0;
+    constructor(address _arbiter, address _beneficiary) payable {
+        arbiter = _arbiter;
+        beneficiary = _beneficiary;
+        depositor = msg.sender;
+        initialDeposit = msg.value;
+
+        gateway.depositETH{value: address(this).balance}(address(this), referralCode);
+    }
+
+    receive() external payable {}
+
+    function approve() external {
+        require(msg.sender == arbiter);
+
+        uint balance = aWETH.balanceOf(address(this));
+        aWETH.approve(address(gateway), balance);
+
+        gateway.withdrawETH(balance, address(this));
+        payable(beneficiary).transfer(initialDeposit);
+        payable(depositor).transfer(address(this).balance);
+        // depositor get interest
+    }
 }
 
-contract EtherEscrow is Initializable{
-  address arbiter;
-  address depositor;
-  address beneficiary;
-  uint initialDeposit;
-  IWETHGateway gateway = IWETHGateway(0xA61ca04DF33B72b235a8A28CfB535bb7A5271B70);
-  IERC20 aWETH = IERC20(0x87b1f4cf9BD63f7BBD3eE1aD04E8F52540349347);
-
-  function initialize (address _arb, address _ben) public payable {
-    arbiter = _arb;
-    beneficiary = _ben;
-    initialDeposit = msg.value;
-    depositor = msg.sender;
-    gateway.depositETH{value: address(this).balance}(address(this), 0);
-  }
-
-  receive() external payable {}
-
-  function withdraw() external {
-    uint balance = aWETH.balanceOf(address(this));
-    aWETH.approve(address(gateway), balance);
-
-    gateway.withdrawETH(balance, address(this));
-
-    payable(beneficiary).transfer(initialDeposit);
-    selfdestruct(payable(depositor));
-  }
-}
+// TODO - Switch to WETHGateway V2; Move comments to README. 
